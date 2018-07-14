4c1fdb124f5ffe0baaac2365f8bda6bad1f6c84f
MSHADroo <mshadroo@gmail.com>
2018-07-15 01:11:01 +0430

    first commit

--- app/Http/Controllers/RulesController.php
+++ app/Http/Controllers/RulesController.php
@@ -6,36 +6,58 @@ use Illuminate\Http\Request;
 
 class RulesController extends Controller
 {
-    public function makeRules($device,$selected){
+    public function makeRules($device, $selected)
+    {
 
         $selectedDevice = [];
-        foreach ($selected as $key => $value){
-            if((int)$value > 0){
+        foreach ($selected as $key => $value) {
+            if ((int)$value > 0) {
                 $selectedDevice[] = $key;
             }
         }
-        dd($selectedDevice);
-        $rulesArray=[];
-        switch ($device){
 
+        $rulesArray = [];
+        switch ($device) {
             case 'cpu':
-                switch ($selected){
-                    case 'mainboard':
-                        $rulesArray = array_merge($rulesArray ,['cpu_socket']);
-                    case 'memory':
-                        $rulesArray = array_merge($rulesArray ,['bus_speed']);
+                foreach ($selectedDevice as $item) {
+                    switch ($item) {
+                        case 'mainboard':
+                            $rulesArray = array_merge($rulesArray, ['cpu_socket' => 'mainboard']);
+                            break;
+                        case 'memory':
+                            $rulesArray = array_merge($rulesArray, ['bus_speed' => 'memory']);
+                            break;
+                    }
                 }
+                break;
+
 
             case 'mainboard':
-                switch ($selected){
-                    case 'cpu':
-                        $rulesArray = array_merge($rulesArray ,['cpu_socket']);
-                    case 'memory':
-                        $rulesArray = array_merge($rulesArray ,['bus_speed']);
+                foreach ($selectedDevice as $item) {
+                    switch ($item) {
+                        case 'cpu':
+                            $rulesArray = array_merge($rulesArray, ['cpu_socket' => 'cpu']);
+                            break;
+
+                        case 'memory':
+                            $rulesArray = array_merge($rulesArray, ['bus_speed' => 'memory']);
+                            break;
+                    }
                 }
+                break;
         }
 
-        return $rulesArray;
+        return $this->findParamsValue($rulesArray, $selected);
 
+    }
+
+
+    public function findParamsValue($rulesArray, $selected)
+    {
+//        dd($rulesArray, $selected);
+
+        foreach ($rulesArray as $rules){
+            dd($selected[$rules[1]]);
+        }
     }
 }
